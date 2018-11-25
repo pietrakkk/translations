9f2448e71d430dd3303aaf2d5665d51c1f6f3be0
Łukasz Piotrkowski <lukasz.piotrkowski@profil-software.com>
2018-11-25 21:25:15 +0100

    Add CORS, directories reorganizations

---
+++ translations-api/app.py
@@ -0,0 +1,15 @@
+import os
+
+from flask import Flask
+from flask_cors import CORS
+
+from apis import api
+
+translator_app = Flask(__name__)
+
+translator_app.config['SAVE_DIRECTORY'] = os.path.join(translator_app.root_path, 'temp')
+CORS(translator_app)
+api.init_app(translator_app)
+
+if __name__ == '__main__':
+    translator_app.run(debug=False)
