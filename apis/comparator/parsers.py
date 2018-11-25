2b97960bf2ebc6a9afb2b3a959dee52e6a3cc83c
Łukasz Piotrkowski <lukasz.piotrkowski@profil-software.com>
2018-11-25 21:26:23 +0100

    Remove unused files

--- apis/comparator/parsers.py
+++
@@ -1,6 +0,0 @@
-from flask_restplus import reqparse
-from werkzeug.datastructures import FileStorage
-
-upload_parser = reqparse.RequestParser()
-upload_parser.add_argument('valid_translation', location='files', type=FileStorage, required=True)
-upload_parser.add_argument('translation_to_fix', location='files', type=FileStorage, required=True)
